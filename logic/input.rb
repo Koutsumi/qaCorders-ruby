5d252864e25f31850eac0a0afdcef3ea08913b10
koutsumi <febaccarini@gmail.com>
2023-02-13 21:29:01 -0300

    first commit

---
+++ logic/input.rb
@@ -0,0 +1,9 @@
+puts "Informe seu nome:"
+name = gets.chomp
+
+# .chomp evita que o programa leia o "enter" durante o input(gets)
+
+puts "Informe seu sobrenome:"
+lastName = gets.chomp
+
+puts "#{name} #{lastName}"
---
+++ logic/varibles.rb
@@ -0,0 +1,29 @@
+# * ------ criando variáveis ------
+# string
+    # aspas simples = referenciar diretório
+    # aspas duplas = texto/string
+first_text = "Qa"
+second_text = "Coders"
+
+# int
+number = 1
+
+#float/decimal
+dec = 1.2
+
+#boolean
+boo_true = true
+boo_false = false
+
+#PUTS imprime o valor no terminal 
+    # para rodar o código - ruby nomeDoArquivo.rb
+puts "Olá mundo!"
+puts "--------------------------------------------"
+puts "Interpolação"
+puts "#{first_text} #{second_text}"
+puts "--------------------------------------------"
+puts "Soma de números"
+puts number + dec 
+puts "--------------------------------------------"
+puts "Valor booleano"
+puts "#{boo_true} | #{boo_false} "
